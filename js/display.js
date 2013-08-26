58aa106a4e8b739e26980afd0c67838451a74e55
schnej7 <schnej7@rpi.edu>
2013-08-26 11:21:21 -0400

    added display code

---
+++ js/display.js
@@ -0,0 +1,71 @@
+/* TO USE:
+// create the display object
+var display = new Display( 64, 32, 12, !CanvasRenderingContext2D )
+// clear screen
+display.fill(0).flush();
+// add to the DOM
+document.getElementById('screen').appendChild( display.container );
+*/
+
+function Display( width, height, pixelSize, canvas ){
+    /* CONSTRUCTOR */
+    var api = this,
+        length = width * height,
+        buffer = [],
+    if( !canvas ){
+        var container = document.createElement('div'),
+            pixel = document.createElement('div'),
+            pixels = container.childNodes;
+        container.style.width = width * pixelSize + 'px';
+        container.style.height = height * pixelSize + 'px';
+        pixel.style.width = pixel.style.height = pixelSize + 'px';
+        pixel.style.cssFloat = pixel.style.styleFloat = 'left';
+        for( var i = 0; i < length; i++ )
+            container.appendChild( pixel.cloneNode() );
+    } else {
+        var container = document.createElement('canvas'),
+            context = container.getContext('2d');
+        container.width = width * pixelSize;
+        container.height = height * pixelSize;
+    }
+    /* PUBLIC VARIABLES AND FUNCTIONS */
+    api.container = container;
+    api.fill = function( value ){
+        for( var i = 0; i < length; i++ )
+            buffer[i] = value;
+        return api;
+    };
+    api.flush = !canvas ?
+        function( _x, _y, w, h ){
+            var flushHeight = ( h + y ) || height;
+            var flushWidth = ( w + x ) || width;
+            for( var y = _y || 0; y < flushHeight; y++ ){
+                for( var x = _x || 0; x < flushWidth; x++ ){
+                    pixels[ y * width + x ].style.backgroundColor = buffer[ y * width + x ];
+                }
+            }
+            return api;
+            
+        } :
+        function( _x, _y, w, h ){
+            var flushHeight = ( h + y ) || height;
+            var flushWidth = ( w + x ) || width;
+            for( var y = _y || 0; y < flushHeight; y++ ){
+                for( var x = _x || 0; x < flushWidth; x++ ){
+                    context.fillStyle = buffer[ y * width + x ];
+                    context.fillRect(
+                        x * pixelSize, y * pixelSize,
+                        pixelSize, pixelSize );
+                }
+            }
+            return api;
+        };
+    api.setPixel = function( x, y, value ){
+        buffer[ (y * width + x) % length ] = value;
+        return api;
+    };
+    api.getPixel = function( x, y ){
+        return buffer[ (y * width + x) % length ];
+    };
+    return api;
+};
